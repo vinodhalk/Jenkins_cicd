076a76895f646b34e387e1b8ccfbf9dbe45db03e
vinodhsfb <38052819+vinodhsfb@users.noreply.github.com>
2018-11-02 15:59:39 -0500

    Add files via upload

---
+++ jenkins_cicd_test.js
@@ -0,0 +1,20 @@
+/**
+ * The ExpressJS namespace.
+ * @external ExpressApplicationObject
+ * @see {@link http://expressjs.com/3x/api.html#app}
+ */ 
+
+/**
+ * Mobile Cloud custom code service entry point.
+ * @param {external:ExpressApplicationObject}
+ * service 
+ */
+module.exports = function(service) {
+
+
+	/**
+	 *  The file samples.txt in the archive that this file was packaged with contains some example code.
+	 */
+
+
+};
